88d3d7d8c8d3cf593dc1a29f718b7ce4abe66bff
Nanhe KumarAdithyaGopakumar <adithyagopa@gmail.com>
2023-08-15 11:31:19 +0530

    delete bookings completed

--- src/pages/bookings.js
+++ src/pages/bookings.js
@@ -1,23 +1,31 @@
-import React, { useState,useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import DataTable from 'react-data-table-component';
-import { Container } from 'reactstrap';
+import { Container, Button, Modal, ModalBody } from 'reactstrap';
 import AuthContext from '../context/auth.context';
 
 const Booking = () => {
 
   const [bookingsList, setBookingsList] = useState([])
+  const [selectedBookingID, setSelectedBookingID] = useState(null)
+  const [isModal, setIsModal] = useState(false)
+
   const context = useContext(AuthContext)
-  console.warn({bookingsList})
+  console.warn({ bookingsList })
 
-  useEffect(()=>{
+  const toggleModal = () => {
+    setIsModal(!isModal);
+  };
+
+  useEffect(() => {
     fetchEvents()
-  },[])
+  }, [])
 
-  const fetchEvents =()=>{
+  const fetchEvents = () => {
     const requestBody = {
       query: `
       query{
         bookings{
+          _id
           event_id{
             title
           }
@@ -48,6 +56,40 @@ const Booking = () => {
       .catch((err) => { console.log(err); })
   }
 
+  const deleteBooking = (id) => {
+    const requestBody = {
+      query: `
+      mutation{
+        cancelBooking( booking_id : "${id}"){
+          event{
+            title
+          }
+          message
+        }
+      }
+    `
+    }
+    fetch("http://localhost:5000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${context.token}`
+      },
+    }).then((res) => {
+      if (res.status !== 200 && res.status !== 200) {
+        throw new Error("Failed")
+      }
+      return res.json()
+    }).then((data) => {
+      fetchEvents()
+      toggleModal()
+      setSelectedBookingID(null)
+      console.log(data);
+    })
+      .catch((err) => { console.log(err); })
+  }
+
   const columns = [
     {
       name: 'Event',
@@ -61,6 +103,17 @@ const Booking = () => {
       name: 'Created At',
       selector: row => row.createdAt,
     },
+    {
+      name: 'Action',
+      cell: row => (
+        <Button
+          onClick={() => {
+            setSelectedBookingID(row._id)
+            setIsModal(true)
+          }}
+          className='btn btn-danger text-white'>Cancel</Button>
+      ),
+    }
   ];
 
   return (
@@ -71,6 +124,19 @@ const Booking = () => {
         data={bookingsList}
         pagination
       />
+       <Modal isOpen={isModal} toggle={toggleModal}>
+        <ModalBody>
+          Are you sure you want to cancel this booking?
+        </ModalBody>
+        <div className="modal-footer">
+          <Button color="secondary" onClick={()=>toggleModal()}>
+            Cancel
+          </Button>
+          <Button color="danger" onClick={() => deleteBooking(selectedBookingID)}>
+            Cancel
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
